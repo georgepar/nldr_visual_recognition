4c10499363613730884afa04b26b392a1dc04ec4
Efthymios Tzinis <etzinis@gmail.com>
2018-05-12 19:33:54 +0300

    Load IEMOCAP data features from both linear and RQA sets for all sentences

---
+++ dataloader/fused_features_IEMOCAP.py
@@ -0,0 +1,89 @@
+"""!
+\brief Get fused features table from a list of dictionaries which
+will be loaded using joblib.
+
+@author Efthymios Tzinis {etzinis@gmail.com}
+@copyright National Technical University of Athens
+"""
+
+import argparse
+from sklearn.externals import joblib
+import numpy as np
+
+
+def convert_2_numpy_per_utterance(dataset_dic):
+    converted_dic = {}
+    for spkr in dataset_dic:
+        x_list = []
+        y_list = []
+        converted_dic[spkr] = {}
+        for id, el_dic in dataset_dic[spkr].items():
+            label = el_dic['y']
+            feat_vec = el_dic['x']
+            x_list.append(feat_vec)
+            y_list.append(label)
+
+        this_utt_array = np.array(x_list)
+        converted_dic[spkr]['x']=this_utt_array
+        converted_dic[spkr]['y']=y_list
+
+    return converted_dic
+
+
+def get_fused_features(list_of_paths):
+    """!
+    \brief Load feature dicts from the respective paths and combine
+    them for acquiring the final numpy arrays"""
+    try:
+        feat_p = list_of_paths.pop(0)
+        final_data_dic = joblib.load(feat_p)
+    except Exception as e:
+        print "At least one file path is required"
+        raise e
+
+    while list_of_paths:
+        feat_p = list_of_paths.pop(0)
+        temp_dic = joblib.load(feat_p)
+        try:
+            for spkr in temp_dic:
+                for id, el_dic in temp_dic[spkr].items():
+                    assert el_dic['y'] == final_data_dic[spkr][id]['y']
+                    prev_vec = final_data_dic[spkr][id]['x']
+                    this_vec = el_dic['x']
+                    new_vec = np.concatenate([prev_vec, this_vec],
+                                             axis=0)
+                    final_data_dic[spkr][id]['x'] = new_vec
+        except Exception as e:
+            print "Failed to update the Fused dictionary"
+            raise e
+
+    converted_dic = convert_2_numpy_per_utterance(final_data_dic)
+
+    x_tr_list = []
+    Y_tr = []
+    for tr_speaker, tr_data in converted_dic.items():
+        x_tr_list.append(tr_data['x'])
+        Y_tr += tr_data['y']
+    X_tr = np.concatenate(x_tr_list, axis=0)
+
+    return X_tr, Y_tr
+
+
+def get_args():
+    """! Command line parser for Utterance level classification Leave
+    one speaker out schema pipeline"""
+    parser = argparse.ArgumentParser(
+        description='Utterance level classification Leave one '
+                    'speaker out schema pipeline' )
+    parser.add_argument('-i', '--input_features_paths', nargs='+',
+                        help='File paths of the features you want to '
+                             'concatenate and the classify')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    """!brief Example of usage"""
+    args = get_args()
+    X, Y = get_fused_features(args.input_features_paths)
+    print X.shape
